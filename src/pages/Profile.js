b0b570157f18780c59b8da34f4bc26a1218a7e53
astrsm <ddefrito84@gmail.com>
2023-06-19 22:37:56 +0700

    refactor: changing image picker package

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -5,11 +5,12 @@ import ViewShot from 'react-native-view-shot';
 import styled from 'styled-components/native';
 import {Buttons} from '../components/DetailButtons';
 import {SignatureOnly} from '../components/Signature';
-import Title from '../components/Title';
+import Title from '../components/Titles';
 import {Body2, Body5, Display2, Display4} from '../components/typographies';
 import {color} from '../styles/variables';
 import RNFS from 'react-native-fs';
 import {launchImageLibrary} from 'react-native-image-picker';
+import ImagePicker from 'react-native-image-crop-picker';
 
 const Container = styled.View`
   padding: 0 ${moderateScale(24)}px;
@@ -20,7 +21,6 @@ const Container = styled.View`
 const ProfileContainer = styled.View`
   flex-direction: row;
   gap: ${moderateScale(24)}px;
-  margin-top: ${moderateScale(32)}px;
 `;
 
 const YLine = styled.View`
@@ -31,6 +31,7 @@ const YLine = styled.View`
 
 const ProfileBody = styled.View`
   flex-direction: column;
+  flex: 1;
   gap: ${moderateScale(8)}px;
 `;
 
@@ -70,26 +71,34 @@ export default function Profile({navigation}) {
   };
 
   const pickImage = async () => {
-    const options = {
-      mediaType: 'photo',
-      includeBase64: false,
-      maxHeight: 200,
-      maxWidth: 200,
-    };
-    const result = await launchImageLibrary(options, response => {
-      if (response.didCancel) {
-        setErrorText('User cancelled image picker');
-      } else if (response.errorCode === 'camera_unavailable') {
-        setErrorText('Camera not available on device');
-      } else if (response.errorCode === 'permission') {
-        setErrorText('Permission not satisfied');
-      } else if (response.errorCode === 'others') {
-        setErrorText(response.errorMessage);
-      }
+    // const options = {
+    //   mediaType: 'photo',
+    //   includeBase64: false,
+    //   maxHeight: 200,
+    //   maxWidth: 200,
+    // };
+    // const result = await launchImageLibrary(options, response => {
+    //   if (response.didCancel) {
+    //     setErrorText('User cancelled image picker');
+    //   } else if (response.errorCode === 'camera_unavailable') {
+    //     setErrorText('Camera not available on device');
+    //   } else if (response.errorCode === 'permission') {
+    //     setErrorText('Permission not satisfied');
+    //   } else if (response.errorCode === 'others') {
+    //     setErrorText(response.errorMessage);
+    //   }
+    // });
+
+    const result = await ImagePicker.openPicker({
+      width: 200,
+      height: 200,
+      cropping: true,
     });
 
+    console.log(result);
+
     if (result) {
-      setNewUri(result.assets[0].uri);
+      setNewUri(result.path);
       showToast('Signature changed');
     }
   };
@@ -112,18 +121,15 @@ export default function Profile({navigation}) {
     <Container>
       <StatusBar backgroundColor={theme} />
       <Title title="Detail" lineColor={theme} />
-      <ProfileContainer>
-        <YLine theme={theme} />
-        <ProfileComponent
-          id={profile.id}
-          name={profile.name}
-          email={profile.email}
-          birthday={date}
-          institution={profile.institution}
-          description={profile.description}
-          theme={theme}
-        />
-      </ProfileContainer>
+      <ProfileComponent
+        id={profile.id}
+        name={profile.name}
+        email={profile.email}
+        birthday={date}
+        institution={profile.institution}
+        description={profile.description}
+        theme={theme}
+      />
       <ShotContainer>
         <ViewShot
           ref={ref}
@@ -177,24 +183,27 @@ export const ProfileComponent = ({
   theme,
 }) => {
   return (
-    <ProfileBody>
-      <View>
-        <Body2>{`#ID-${id}`}</Body2>
-        <Display2>{name}</Display2>
-        <Display4>{email}</Display4>
-      </View>
-      <View>
-        <Body2 color={theme}>Birthday</Body2>
-        <Body5>{birthday}</Body5>
-      </View>
-      <View>
-        <Body2 color={theme}>Institution</Body2>
-        <Body5>{institution}</Body5>
-      </View>
-      <View>
-        <Body2 color={theme}>Description</Body2>
-        <Body5>{description}</Body5>
-      </View>
-    </ProfileBody>
+    <ProfileContainer>
+      <YLine theme={theme} />
+      <ProfileBody>
+        <View>
+          <Body2>{`#ID-${id}`}</Body2>
+          <Display2>{name}</Display2>
+          <Display4>{email}</Display4>
+        </View>
+        <View>
+          <Body2 color={theme}>Birthday</Body2>
+          <Body5>{birthday}</Body5>
+        </View>
+        <View>
+          <Body2 color={theme}>Institution</Body2>
+          <Body5>{institution}</Body5>
+        </View>
+        <View>
+          <Body2 color={theme}>Description</Body2>
+          <Body5>{description}</Body5>
+        </View>
+      </ProfileBody>
+    </ProfileContainer>
   );
 };
